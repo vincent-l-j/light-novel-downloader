5c533619f35af35bf504505b114e023b1d28818e
Vincent Jadraque <vincent.jadraque@dfcrc.com>
2024-07-25 13:54:30 +1000

    feat: run package as module

---
+++ src/light_novel_downloader/__init__.py
@@ -0,0 +1,15 @@
+def main():
+    from .scraper import Scraper
+
+    source = "https://novelfull.com"
+    path = "/reincarnation-of-the-strongest-sword-god/chapter-1-starting-over.html"
+    dir_downloads = "downloads"
+    scraper = Scraper()
+    r = scraper.get_response(source + path)
+    page_name = f"{dir_downloads}{path}"
+    with open(page_name, "w") as f_out:
+        f_out.write(r.text)
+
+
+if __name__ == "__main__":
+    main()
--- src/light_novel_downloader/__main__.py
+++ src/light_novel_downloader/__main__.py
@@ -1,11 +1,10 @@
-from src.light_novel_downloader.scraper import Scraper
+import sys
+from . import main
 
-
-source = "https://novelfull.com"
-path = "/reincarnation-of-the-strongest-sword-god/chapter-1-starting-over.html"
-dir_downloads = "downloads"
-scraper = Scraper()
-r = scraper.get_response(source + path)
-page_name = f"{dir_downloads}{path}"
-with open(page_name, "w") as f_out:
-    f_out.write(r.text)
+exit_status = 1
+try:
+    main()
+    exit_status = 0
+except Exception as e:
+    print("Error:", e, file=sys.stderr)
+sys.exit(exit_status)
