5678c305ec50463d3826d83c705d6aa1dc2c3f07
Vincent Jadraque <vincent.jadraque@dfcrc.com>
2024-08-21 10:30:39 +1000

    fix: create downloads folder if necessary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.light_novel_downloader.scraper import Scraper
+from pathlib import Path
 
 source = "https://novelfull.com"
 path = "/reincarnation-of-the-strongest-sword-god/chapter-1-starting-over.html"
@@ -6,5 +7,7 @@ dir_downloads = "downloads"
 scraper = Scraper()
 r = scraper.get_response(source + path)
 page_name = f"{dir_downloads}{path}"
+dir_novel = "/".join(page_name.split("/")[:-1])
+Path(dir_novel).mkdir(parents=True, exist_ok=True)
 with open(page_name, "w") as f_out:
     f_out.write(r.text)
