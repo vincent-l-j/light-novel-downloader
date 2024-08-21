d4703bb1c3efe16fc9358661fa950975d3504bd0
Vincent Jadraque <vincent.jadraque@dfcrc.com>
2024-08-21 10:37:50 +1000

    feat: convert into command line interface using click

---
+++ tests/test_light_novel_downloader.py
@@ -0,0 +1,26 @@
+from click.testing import CliRunner
+from light_novel_downloader.cli import cli
+
+
+def test_version():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["--version"])
+        assert result.exit_code == 0
+        assert result.output.startswith("cli, version ")
+
+
+def test_help():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["--help"])
+        assert result.exit_code == 0
+        assert result.output.startswith("Usage: ")
+
+
+def test_download():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["download"])
+        assert result.exit_code == 0
+        assert result.output.startswith("Downloaded chapter")
